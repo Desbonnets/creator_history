ae6db1f72b55be96b76ebb6a7acdf318c52c80b4
Gilles DESBONNETS <gillesdesbonnets@gmail.com>
2026-03-07 19:20:07 +0100

    [FEAT] ajout de la page d'accueil et du css

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { StoreProvider } from './store/StoreContext'
+import Layout from './components/Layout'
+import HomePage from './pages/HomePage'
+import UniversePage from './pages/UniversePage'
+import UniverseSettingsPage from './pages/UniverseSettingsPage'
+import StoriesPage from './pages/StoriesPage'
+import StoryFormPage from './pages/StoryFormPage'
+import ElementListPage from './pages/ElementListPage'
+import ElementFormPage from './pages/ElementFormPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <StoreProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="universe/:id" element={<UniversePage />} />
+            <Route path="universe/:id/settings" element={<UniverseSettingsPage />} />
+            <Route path="universe/:id/stories" element={<StoriesPage />} />
+            <Route path="universe/:id/story/new" element={<StoryFormPage />} />
+            <Route path="universe/:id/story/:storyId" element={<StoryFormPage />} />
+            <Route path="universe/:id/:type" element={<ElementListPage />} />
+            <Route path="universe/:id/:type/new" element={<ElementFormPage />} />
+            <Route path="universe/:id/:type/:elementId" element={<ElementFormPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </StoreProvider>
   )
 }
 
